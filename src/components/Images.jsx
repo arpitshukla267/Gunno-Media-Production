311a7f3a404f1a7e4cf1b6882213785ecda1229a
Arpit Shukla <shuklaarpit440@email.com>
2025-06-17 15:52:00 +0530

    Fix case-sensitive import for Cursor

--- src/components/Images.jsx
+++ src/components/Images.jsx
@@ -1,39 +1,39 @@
 // From ModelPhtoshoot
 
-import m11 from '../assets/imagecompressor (1)/IMG_0743a-min.jpg';
-import m12 from '../assets/imagecompressor (1)/IMG_0759we-min.jpg';
-import m13 from '../assets/imagecompressor (1)/IMG_0767E-min.jpg';
-import m14 from '../assets/imagecompressor (1)/IMG_0772-min.jpg';
-import m15 from '../assets/imagecompressor (1)/IMG_0784aa-min.jpg';
-import m21 from '../assets/imagecompressor (1)/IMG_0787Ea-min.jpg';
-import m22 from '../assets/imagecompressor (1)/IMG_0796Eaw-min.jpg';
-import m23 from '../assets/imagecompressor (1)/IMG_0860ea-min.jpg';
-import m24 from '../assets/imagecompressor (1)/IMG_0860Ew-min.jpg';
-import m25 from '../assets/imagecompressor (1)/IMG_0874Ene-min.jpg';
+import m11 from '../assets/imagecompressor-1/IMG_0743a-min.jpg';
+import m12 from '../assets/imagecompressor-1/IMG_0759we-min.jpg';
+import m13 from '../assets/imagecompressor-1/IMG_0767E-min.jpg';
+import m14 from '../assets/imagecompressor-1/IMG_0772-min.jpg';
+import m15 from '../assets/imagecompressor-1/IMG_0784aa-min.jpg';
+import m21 from '../assets/imagecompressor-1/IMG_0787Ea-min.jpg';
+import m22 from '../assets/imagecompressor-1/IMG_0796Eaw-min.jpg';
+import m23 from '../assets/imagecompressor-1/IMG_0860ea-min.jpg';
+import m24 from '../assets/imagecompressor-1/IMG_0860Ew-min.jpg';
+import m25 from '../assets/imagecompressor-1/IMG_0874Ene-min.jpg';
 
 // From ModelPhtoshoot2
-import m6 from '../assets/imagecompressor (1)/IMG_0472-min.jpeg';
-import m7 from '../assets/imagecompressor (1)/IMG_0512-min.jpeg';
-import m8 from '../assets/imagecompressor (1)/IMG_0522-min.jpeg';
-import m9 from '../assets/imagecompressor (1)/IMG_0689-min.jpg';
-import m10 from '../assets/imagecompressor (1)/IMG_0725-min.jpg';
-import m16 from '../assets/imagecompressor (1)/IMG_0270-min.jpg';
-import m17 from '../assets/imagecompressor (1)/IMG_0335-min.jpg';
-import m18 from '../assets/imagecompressor (1)/IMG_0348-min.jpg';
-import m19 from '../assets/imagecompressor (1)/IMG_0467-min.jpeg';
-import m20 from '../assets/imagecompressor (1)/IMG_0470-min.jpeg';
-import m26 from '../assets/imagecompressor (1)/IMG_1861-min.jpg';
-import m27 from '../assets/imagecompressor (1)/IMG_1860-min.jpg';
-import m28 from '../assets/imagecompressor (1)/IMG_1842-min.jpeg';
-import m29 from '../assets/imagecompressor (1)/IMG_0725-min.jpg'; 
-import m30 from '../assets/imagecompressor (1)/IMG_0689-min.jpg';  
+import m6 from '../assets/imagecompressor-1/IMG_0472-min.jpeg';
+import m7 from '../assets/imagecompressor-1/IMG_0512-min.jpeg';
+import m8 from '../assets/imagecompressor-1/IMG_0522-min.jpeg';
+import m9 from '../assets/imagecompressor-1/IMG_0689-min.jpg';
+import m10 from '../assets/imagecompressor-1/IMG_0725-min.jpg';
+import m16 from '../assets/imagecompressor-1/IMG_0270-min.jpg';
+import m17 from '../assets/imagecompressor-1/IMG_0335-min.jpg';
+import m18 from '../assets/imagecompressor-1/IMG_0348-min.jpg';
+import m19 from '../assets/imagecompressor-1/IMG_0467-min.jpeg';
+import m20 from '../assets/imagecompressor-1/IMG_0470-min.jpeg';
+import m26 from '../assets/imagecompressor-1/IMG_1861-min.jpg';
+import m27 from '../assets/imagecompressor-1/IMG_1860-min.jpg';
+import m28 from '../assets/imagecompressor-1/IMG_1842-min.jpeg';
+import m29 from '../assets/imagecompressor-1/IMG_0725-min.jpg'; 
+import m30 from '../assets/imagecompressor-1/IMG_0689-min.jpg';  
 
 // Compressed images
-import m1 from '../assets/imagecompressor (1)/Aa-min.jpg';
-import m2 from '../assets/imagecompressor (1)/IMG_0675-min.jpg';
-import m3 from '../assets/imagecompressor (1)/IMG_0685E-min.jpg';
-import m4 from '../assets/imagecompressor (1)/IMG_0687-min.jpg';
-import m5 from '../assets/imagecompressor (1)/IMG_0698eaa-min.jpg';
+import m1 from '../assets/imagecompressor-1/Aa-min.jpg';
+import m2 from '../assets/imagecompressor-1/IMG_0675-min.jpg';
+import m3 from '../assets/imagecompressor-1/IMG_0685E-min.jpg';
+import m4 from '../assets/imagecompressor-1/IMG_0687-min.jpg';
+import m5 from '../assets/imagecompressor-1/IMG_0698eaa-min.jpg';
 
 import m31 from '../assets/PhoneLink/IMG-20250612-WA0003.jpg';
 import m32 from '../assets/PhoneLink/IMG-20250612-WA0004.jpg';
